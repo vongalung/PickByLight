5414a69e7bc1b0f2757cf23139453b0d1d3fcae0
ARTHUR HUTAGALUNG <45054498+vongalung@users.noreply.github.com>
2018-11-24 16:43:39 +0700

    Update py_pbl.sh
    Replace shell script with bash script, for error logging. From now on most of the python errors shall be reported in logs/error.log, while the rest (including OS/system errors) shall be reported in logs/system.log

--- py_pbl.sh
+++ py_pbl.sh
@@ -1,6 +1,15 @@
-#!/bin/sh
-ROOT=$(pwd)
-PATH="${ROOT}/bin/:{$PATH}"
-script='${ROOT}/main.py'
+#!/bin/bash
+BASE_DIR=$(dirname "$0")
+LOGFILE="${BASE_DIR}/logs/system.log"
+
+coproc errorHandling {
+        ( bash -c 'while read line; do echo $(date): ${line}; done' 1>>$LOGFILE )
+}
+
+exec 2>&${errorHandling[1]}
+
+script="${BASE_DIR}/main.py"
+source "${BASE_DIR}/bin/activate" &
 python $script &
+
 exit 0
